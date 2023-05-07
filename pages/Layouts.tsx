d98abd5ce56dfbdc96d61ef92d9edd91aa8829a1
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2023-05-07 18:28:24 +0200

    Update Layouts.tsx

--- pages/Layouts.tsx
+++ pages/Layouts.tsx
@@ -21,7 +21,7 @@ const Layouts = () => {
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 1 }} className="overflow-y-scroll">
-      <nav className="flex items-center justify-center overflow-x-auto text-md">
+      <nav className="flex items-center justify-center overflow-x-auto text-md sm:text-sm xs:text-xs">
         <Link href="/" className="p-1">   
         </Link>
         <Link href="/" className="p-1">   
